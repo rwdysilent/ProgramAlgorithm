b7ea1a519527742aac4826ff24ffa10667f66284
wupengfei <wupengfei@sogou-inc.com>
2018-03-19 23:37:55 +0800

    heapsort 待学习

---
+++ golang/排序3.go
@@ -0,0 +1,21 @@
+// Copyright 2018 pfwu. All rights reserved.
+//
+// @Author: pfwu
+// @Date: 2018-03-18 00:10
+
+package main
+
+import (
+	"fmt"
+)
+
+var dData = []int{33, 5, 76, 4, 34, 32, 6}
+
+//堆排序
+func heapSort(){
+
+}
+
+func main(){
+	fmt.Println()
+}
