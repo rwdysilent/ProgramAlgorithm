e52bc2c12df10d883f78dd557de1082200f6c425
wupengfei <wupengfei@sogou-inc.com>
2018-04-10 23:42:04 +0800

    link cycle

--- python/链表回环判断.py
+++ python/链表回环判断.py
@@ -2,6 +2,14 @@
 # @author: pfwu
 # @Date: 2018-04-10 23:19
 
+# https://leetcode.com/problems/linked-list-cycle/description/
+#
+# Given a linked list, determine if it has a cycle in it.
+#
+# Follow up:
+# Can you solve it without using extra space
+# 思路：设置两个指针，一个快指针，一个慢指针，快指针每次移动2个位置，慢指针每次移动1个位置，如果是回环链表，俩指针必定会相遇
+
 
 class Solution(object):
     def hasCycle(self, head):
