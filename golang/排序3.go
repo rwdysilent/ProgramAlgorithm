11dae37b12345d4ada978ccc6635f968dd787153
wupengfei <wupengfei@sogou-inc.com>
2018-04-25 11:51:12 +0800

    堆排序

--- golang/排序3.go
+++ golang/排序3.go
@@ -9,13 +9,49 @@ import (
 	"fmt"
 )
 
-var dData = []int{33, 5, 76, 4, 34, 32, 6}
-
 //堆排序
-func heapSort(){
-
+func HeapSort(array []int) {
+	m := len(array)
+	s := m / 2
+	for i := s; i > -1; i-- {
+		heap(array, i, m-1)
+	}
+	for i := m - 1; i > 0; i-- {
+		array[i], array[0] = array[0], array[i]
+		heap(array, 0, i-1)
+	}
 }
 
-func main(){
-	fmt.Println()
+func heap(array []int, i, end int) {
+	//左子树
+	l := 2*i + 1
+	if l > end {
+		return
+	}
+
+	n := l
+
+	//右子数
+	r := 2*i + 2
+	if r <= end && array[r] > array[l] {
+		n = r
+	}
+
+	//根节点大于左子树，返回
+	if array[i] > array[n] {
+		return
+	}
+
+	array[n], array[i] = array[i], array[n]
+
+	fmt.Println("arry: ", array)
+	heap(array, n, end)
+}
+
+func main() {
+	dData := []int{
+		55, 94, 87, 1, 4, 32, 11, 77, 39, 42, 64, 53, 70, 12, 9,
+	}
+	HeapSort(dData)
+	fmt.Println(dData)
 }
