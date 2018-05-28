3e5f73a3e0522a4b1a79763e5288dd0137453d84
wupengfei <wupengfei@sogou-inc.com>
2018-05-28 23:04:39 +0800

    待完善

---
+++ golang/吃面包.go
@@ -0,0 +1,59 @@
+// Copyright 2018 pfwu. All rights reserved.
+//
+// @Author: pfwu
+// @Date: 2018-04-28 23:33
+
+//n 个面包，两种吃法，一次一个或者一次两个，求出所有可能的序列
+
+//TODO: 未完善
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+var EndAns [][]string
+
+func eatBread(n int) {
+	var ans []string
+	if n < 2 {
+		ans = append(ans, "1")
+		fmt.Println(ans)
+		return
+	}
+	for i := 0; i < n/2; i++ {
+		ans = append(ans, "2")
+	}
+	if n%2 == 0 {
+		fmt.Println(ans)
+		return
+	}
+	ans = append(ans, "1")
+	fmt.Println(ans)
+}
+
+func arrangement(ans []string) {
+	tmp := []string{"1", "1"}
+	for i := range ans {
+		if ans[i] == "2" {
+			var out []string
+			out = append(out, ans[:i]...)
+			out = append(out, tmp...)
+			out = append(out, ans[i+1:]...)
+			fmt.Println(out)
+			arrangement(out)
+		}
+	}
+}
+
+func main() {
+	eatBread(4)
+	arrangement([]string{"2", "2"})
+	fmt.Println("End: ", EndAns)
+
+	fmt.Println(strings.Count("fivefivesasd", "ive"))
+	fmt.Println(strings.HasPrefix("-&-weixin", "&"))
+	fmt.Println(strings.IndexAny("-&weixin", "#"))
+}
