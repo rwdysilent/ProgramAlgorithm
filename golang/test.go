97409d130a579b55bc4f77c32279cfe1ad4c121c
wupengfei <wupengfei@sogou-inc.com>
2018-09-19 19:06:01 +0800

    add test

---
+++ golang/test.go
@@ -0,0 +1,13 @@
+// Copyright 2018 pfwu. All rights reserved.
+//
+// @Author: pfwu
+// @Date: 2018-09-19 18:41
+
+package main
+
+import "fmt"
+
+func main() {
+	s := "string"
+	fmt.Printf("%T", s[1])
+}
