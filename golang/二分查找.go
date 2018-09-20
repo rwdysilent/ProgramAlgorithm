08ed8e8440286456c35e9077223d7a1373db6fcf
wupengfei <wupengfei@sogou-inc.com>
2018-09-20 16:02:12 +0800

    增加注释

--- golang/二分查找.go
+++ golang/二分查找.go
@@ -3,6 +3,11 @@
 // @Author: pfwu
 // @Date: 2018-04-11 18:42
 
+// 二分搜索（英语：binary search），也称折半搜索（英语：half-interval search）[1]、对数搜索（英语：logarithmic search）[2]，
+// 是一种在有序数组中查找某一特定元素的搜索算法。搜索过程从数组的中间元素开始，如果中间元素正好是要查找的元素，则搜索过程结束；如果某一
+// 特定元素大于或者小于中间元素，则在数组大于或小于中间元素的那一半中查找，而且跟开始一样从中间元素开始比较。如果在某一步骤数组为空，则
+// 代表找不到。这种搜索算法每一次比较都使搜索范围缩小一半。
+
 package main
 
 import "fmt"
@@ -23,8 +28,8 @@ func binarySearch(arr []int, low, high, hkey int) int {
 }
 
 func main() {
-	arr := []int{1, 3, 2, 5, 4, 7, 6}
+	arr := []int{1, 2, 3, 4, 5, 6, 7}
 
-	n := binarySearch(arr, 0, 7, 7)
+	n := binarySearch(arr, 0, 7, 4)
 	fmt.Println(n)
 }
