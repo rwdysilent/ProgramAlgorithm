eda95c719183e5858293e9e784720c40e4c325b9
wupengfei <wupengfei@sogou-inc.com>
2018-11-26 17:35:58 +0800

    update

--- golang/吃面包.go
+++ golang/吃面包.go
@@ -4,75 +4,40 @@
 // @Date: 2018-04-28 23:33
 
 //n 个面包，两种吃法，一次一个或者一次两个，求出所有可能的序列
+//动态规划：https://zhuanlan.zhihu.com/p/31628866
 
 //TODO: 未完善
 
 package main
 
-import (
-	"fmt"
-	)
+import "fmt"
 
-var EndAns [][]string
-
-func eatBread(n int) {
-	var ans []string
-	if n < 2 {
-		ans = append(ans, "1")
-		fmt.Println(ans)
-		return
-	}
-	for i := 0; i < n/2; i++ {
-		ans = append(ans, "2")
-	}
-	if n%2 == 0 {
-		fmt.Println(ans)
-		return
-	}
-	ans = append(ans, "1")
-	fmt.Println(ans)
-}
-
-func arrangement(ans []string) {
-	tmp := []string{"1", "1"}
-	for i := range ans {
-		if ans[i] == "2" {
-			var out []string
-			out = append(out, ans[:i]...)
-			out = append(out, tmp...)
-			out = append(out, ans[i+1:]...)
-			fmt.Println(out)
-			arrangement(out)
-		}
-	}
-}
-
-func fab(n int) {
-	var s string = ""
-	myFab(n, s)
-}
-
-func myFab(n int, s string) {
+func FindN(n int) []string {
 	if n == 0 {
-		fmt.Println(s)
-		return
+		return []string{}
 	}
-	if n > 0 {
-		myFab(n-1, s+"1")
+	if n == 1 {
+		return []string{"1"}
 	}
-	if n > 1 {
-		myFab(n-2, s+"2")
+	if n == 2 {
+		return []string{"11", "2"}
 	}
+
+	var tmp []string
+
+	var Get func(n int) []string
+	Get = func(n int) []string {
+		tmp = append([]string{"1"}, Get(n-1)...)
+		tmp = append([]string{"2"}, Get(n-2)...)
+		tmp = append([]string{"11"}, Get(n-2)...)
+		return tmp
+	}
+
+	return tmp
 }
 
 func main() {
-	//eatBread(4)
-	//arrangement([]string{"2", "2"})
-	//fmt.Println("End: ", EndAns)
-	//
-	//fmt.Println(strings.Count("fivefivesasd", "ive"))
-	//fmt.Println(strings.HasPrefix("-&-weixin", "&"))
-	//fmt.Println(strings.IndexAny("-&weixin", "#"))
-
-	myFab(10, "")
+	t := FindN(2)
+	fmt.Println("=========")
+	fmt.Printf("%+v", t)
 }
