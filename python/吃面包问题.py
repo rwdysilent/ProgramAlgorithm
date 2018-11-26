63e57f4bb2029cb654d6e0120d783389ba3659ce
wupengfei <wupengfei@sogou-inc.com>
2018-11-26 17:35:51 +0800

    update

--- python/吃面包问题.py
+++ python/吃面包问题.py
@@ -3,13 +3,17 @@
 # @Date: 2018-04-27 14:26
 
 # N 个面包，分两种吃法，一次吃 1 个或者一次吃 2 个，求所有吃法的序列组合并打印
+# 动态规划：https://zhuanlan.zhihu.com/p/31628866
 
 
 def eat(n):
+    if n == 0:
+        return []
     if n == 1:
         return ['1']
     if n == 2:
         return ['11', '2']
+
     return list(set(
                     list(
                         set(
@@ -28,11 +32,13 @@ def eat(n):
 
 
 if __name__ == '__main__':
-    print(eat(3))
+    # print(eat(4))
+    #
+    # print("----")
+    # print(list(set(map(lambda x: '1' + x, ['1', '2', '1']))))
+    # print(list(set(map(lambda x: '2' + x, eat(3 - 2)))))
+    #
+    # print('---')
+    # print(list(set(map(lambda x: '2' + x, ['1', '2', '1']))))
 
-    print("----")
-    print(list(set(map(lambda x: '1' + x, ['1', '2', '1']))))
-    print(list(set(map(lambda x: '2' + x, eat(3 - 2)))))
-
-    print('---')
-    print(list(set(map(lambda x: '2' + x, ['1', '2', '1']))))
+    print(list(set(map(lambda x: x + '1', ['1', '2', '1']))))
