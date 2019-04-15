1912c26202f32dfa00ff3a34c9392e86ffa2b650
wupengfei <wupengfei@sogou-inc.com>
2019-04-15 15:50:28 +0800

    update

--- python/Move Zeroes.py
+++ python/Move Zeroes.py
@@ -1,14 +1,36 @@
-class Solution(object):
+# leetcode 283. Move Zeroes
+class Solution1(object):
     def moveZeroes(self, nums):
         """
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
         nums.sort(key=lambda x: x == 0)
-        print nums
+        return nums
 
 
+P = Solution1()
+print("Solution1: ", P.moveZeroes(nums=[0, 1, 0, 3, 12]))
 
-P = Solution()
-P.moveZeroes(nums = [0, 1, 0, 3, 12])
-#given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+# given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+
+class Solution2(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        k = 0
+        for i in range(len(nums)):
+            if nums[i]:
+                if k != i:
+                    nums[k], nums[i] = nums[i], nums[k]
+                k += 1
+
+        return nums
+
+
+P = Solution2()
+print("Solution2: ", P.moveZeroes(nums=[0, 1, 0, 3, 12]))
