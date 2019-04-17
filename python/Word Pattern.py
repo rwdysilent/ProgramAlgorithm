8f026e92b5156e6d775ae0b3f78d86b6b57d84c7
wupengfei <wupengfei@sogou-inc.com>
2019-04-18 00:53:16 +0800

    update

--- python/Word Pattern.py
+++ python/Word Pattern.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 class Solution(object):
     def wordPattern(self, pattern, str):
         """
@@ -6,46 +7,40 @@ class Solution(object):
         :type str: str
         :rtype: bool
         """
-        #myself#
-        #d = []
-        #p = list(pattern)
-        #s = str.split(' ')
-        #p.sort()
-        #s.sort()
-        #i = 0
-        #lp = len(p)
-        #ls = len(s)
-        #if lp != ls:
-        #    print False
-        #else:
-        #    while i < lp - 1:
-        #        if p[i] == p[i+1] and s[i] == s[i+1]:
-        #            d.append(True)
-        #        elif p[i] != p[i+1] and s[i] != s[i+1]:
-        #            d.append(True)
-        #        else:
-        #            d.append(False)
-        #        i += 1
-        #    if False in d:
-        #        print False
-        #    else:
-        #        print True
+        p_map = {}
+        p_list = []
+        s_map = {}
+        s_list = []
 
-        #大神巧用map,学习
+        for idx, value in enumerate(pattern):
+            if value in p_map:
+                p_list.append(p_map[value])
+            else:
+                p_map[value] = idx
+                p_list.append(idx)
+
+        for idx, value in enumerate(str.split()):
+            if value in s_map:
+                s_list.append(s_map[value])
+            else:
+                s_map[value] = idx
+                s_list.append(idx)
+
+        if p_list == s_list:
+            return True
+
+        return False
+
+
+class Solution2(object):
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+
+        # 大神巧用map,学习
         s = pattern
         t = str.split()
-        print map(s.find, s) == map(t.index, t)
-
-P = Solution()
-P.wordPattern('abba', "dog cat cat dog")
-P.wordPattern(pattern = "abba", str = "dog cat cat fish")
-P.wordPattern(pattern = "aaaa", str = "dog cat cat dog")
-P.wordPattern(pattern = "abba", str = "dog dog dog dog")
-P.wordPattern(pattern = "aaa", str = "aa aa aa aa")
-
-
-
-#pattern = "abba", str = "dog cat cat dog" should return true.
-#pattern = "abba", str = "dog cat cat fish" should return false.
-#pattern = "aaaa", str = "dog cat cat dog" should return false.
-#pattern = "abba", str = "dog dog dog dog" should return false.
+        print(map(s.find, s) == map(t.index, t))
