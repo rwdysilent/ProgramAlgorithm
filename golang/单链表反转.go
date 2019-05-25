f97c35b2f26252e951639150b8a5150b422e433a
wupengfei09 <wupengfei09@meituan.com>
2019-05-25 12:27:15 +0800

    add

--- golang/单链表反转.go
+++ golang/单链表反转.go
@@ -8,6 +8,32 @@ package main
 
 /**
  * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	if head == nil{
+		return nil
+	}
+
+    var pre *ListNode
+    cur := head
+
+    for cur != nil {
+        next := cur.Next
+
+        cur.Next = pre
+        pre = cur
+        cur = next
+    }
+
+    return pre
+}
+
+/**
+ * Definition for singly-linked list.
 */
 
 type ListNode struct {
---
+++ python/Reverse Linked List.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+# Copyright 2019 wupengfei. All rights reserved.
+# @Author: wupengfei
+# @Date: 2019-05-25 11:37
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        pre = None
+        cur = head
+
+        while cur is not None:
+            next = cur.next
+
+            cur.next = pre
+            pre = cur
+            cur = next
+
+        return pre
