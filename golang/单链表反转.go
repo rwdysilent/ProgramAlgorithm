8bc678102d9738bd8e34aef60a8ce2565e7274a9
wupengfei09 <wupengfei09@meituan.com>
2019-05-29 21:24:12 +0800

    update

--- golang/单链表反转.go
+++ golang/单链表反转.go
@@ -8,33 +8,7 @@ package main
 
 /**
  * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
  */
-func reverseList(head *ListNode) *ListNode {
-	if head == nil{
-		return nil
-	}
-
-    var pre *ListNode
-    cur := head
-
-    for cur != nil {
-        next := cur.Next
-
-        cur.Next = pre
-        pre = cur
-        cur = next
-    }
-
-    return pre
-}
-
-/**
- * Definition for singly-linked list.
-*/
 
 type ListNode struct {
 	Val  int
@@ -45,31 +19,50 @@ func reverseList(head *ListNode) *ListNode {
 	if head == nil {
 		return nil
 	}
-	saveNext := head.Next
-	var next *ListNode = nil
-	for saveNext != nil {
-		head.Next = next
-		next = head
-		head = saveNext
-		saveNext = head.Next
-		if saveNext == nil {
-			head.Next = next
-			return head
-		}
+
+	var pre *ListNode
+	cur := head
+
+	for cur != nil {
+		next := cur.Next
+
+		cur.Next = pre
+		pre = cur
+		cur = next
 	}
 
-	return head
+	return pre
 }
+
+//func reverseList(head *ListNode) *ListNode {
+//	if head == nil {
+//		return nil
+//	}
+//	saveNext := head.Next
+//	var next *ListNode = nil
+//	for saveNext != nil {
+//		head.Next = next
+//		next = head
+//		head = saveNext
+//		saveNext = head.Next
+//		if saveNext == nil {
+//			head.Next = next
+//			return head
+//		}
+//	}
+//
+//	return head
+//}
 
 //练习
 func myReverseList(head *ListNode) *ListNode {
-	if head == nil{
+	if head == nil {
 		return nil
 	}
 
 	q := head.Next
 	var next *ListNode
-	for q != nil{
+	for q != nil {
 		head.Next = next
 		next = head
 		head = q
